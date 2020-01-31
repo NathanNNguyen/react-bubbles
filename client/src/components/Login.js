e1f243c468c2ddb6ade7c5e0125db03d36f95cf5
Nhan <natenguyen51294@gmail.com>
2020-01-30 17:37:51 -0800

    got basic login form render

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,27 @@
 import React from "react";
 
+import './styles.scss';
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form className='form'>
+        <h3>Log in</h3>
+        <label>Username</label>
+        <input 
+        type='text'
+        name='username'
+        placeholder='Username'
+        />
+        <label>Password</label>
+        <input 
+        type='text'
+        name='password'
+        placeholder='Password'
+        />
+        <button>Log In</button>
+      </form>
     </>
   );
 };
